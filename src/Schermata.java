af7680a03a12c4984af3b02c370a739b99340d59
TRMN06 <melchionna.andrea@darzo.net>
2024-04-10 08:38:06 +0200

    aggiunta frame

---
+++ src/Schermata.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class Schermata extends MioFrame implements ActionListener,WindowListener {
+    public Schermata(String titolo){
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+}
