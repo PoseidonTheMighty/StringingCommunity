aeb265c2008a7849d6b6759e9cd801b8ca8667df
Imraj Singh <singh.imraj@darzo.net>
2024-04-15 00:53:21 +0200

    aggiunto pag log(fa schifo lo so)

--- src/Schermata.java
+++ src/Schermata.java
@@ -11,17 +11,39 @@ public class Schermata extends MioFrame implements ActionListener,WindowListener
 
     public Schermata(String titolo){
 
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getContentPane().setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
 
+        JPanel panel = new JPanel();
 
-        GridLayout gl = new GridLayout(0, 4, 2, 2);
-        this.setLayout(gl);
+        JLabel email = new JLabel("E-Mail:");
+        JLabel pass = new JLabel("Password:");
 
-        JButton lg = new JButton("LOGIN");
-        this.add(lg);
+        JTextField input_email = new JTextField();
+        JTextField input_pass = new JTextField();
 
+        JButton log = new JButton("LOGIN");
+        JButton reg = new JButton("REGISTER");
 
-    }
+        panel.add(email);
+        panel.add(input_email);
+        panel.add(pass);
+        panel.add(input_pass);
+        panel.add(log);
+        panel.add(reg);
+        panel.setPreferredSize(new Dimension(400, 100));
+        panel.setMaximumSize(new Dimension(400, 100));
+        panel.setBorder(BorderFactory.createTitledBorder("demo"));
+
+        this.getContentPane().add(panel);
+        this.setSize(550, 300);
+        this.setVisible(true);
+        }
+
     @Override
     public void actionPerformed(ActionEvent e) {
+
     }
 }
+
+
