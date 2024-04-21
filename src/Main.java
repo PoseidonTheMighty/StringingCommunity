166e13e600275be8b199d5e15459d849ab4c97a6
Imraj Singh <singh.imraj@darzo.net>
2024-04-21 08:05:53 +0200

    cambio colore dell'app

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatIntelliJLaf;
 import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
 
 public class Main {
     public static void main(String[] args) {
 
-        FlatLightLaf.setup();
+        FlatMacDarkLaf.setup();
 
         Schermata sc = new Schermata("Stringing Community");
         sc.setBounds(400, 200, 330, 300);
