9c2653c6b322a16f28484107b779b4aa50be7183
PoseidonTheMight <singh.imraj@informatica.lcl>
2024-04-22 10:18:27 +0200

    modifiche grafica

--- src/PswForgot.java
+++ src/PswForgot.java
@@ -12,16 +12,15 @@ import java.io.IOException;
 public class PswForgot extends MioFrame implements ActionListener,WindowListener{
     JTextField t1, t2;
     JButton b1;
-    JLabel l1, l2, l3, l5;
+    JLabel l1, l2, l3, l4, l5;
 
     public PswForgot(){
+        setLayout(null);
 
-
-
-        l3 = new JLabel("Reset Password");
-        l3.setFont(new Font("Times New Roman", Font.BOLD, 30));
-        l3.setForeground(Color.RED);
-        l3.setBounds(75, 20, 300, 40);
+        l3 = new JLabel("Reset");
+        l3.setFont(new Font("Gotham", Font.BOLD, 30));
+        l3.setForeground(Color.white);
+        l3.setBounds(100, 12, 300, 40);
 
         l1 = new JLabel("Email:");
         l1.setBounds(60, 60, 80, 30);
@@ -35,8 +34,12 @@ public class PswForgot extends MioFrame implements ActionListener,WindowListener
         t2 = new JPasswordField(60);
         t2.setBounds(100, 100, 80, 30);
 
-        b1 = new JButton("Sign Up");
-        b1.setBounds(100, 140, 80, 30);
+        b1 = new JButton("Reset");
+        b1.setFont(new Font("Gotham", Font.BOLD, 14));
+        b1.setForeground(Color.black);
+        b1.setBackground(Color.white);
+        b1.setBounds(90, 140, 100, 33);
+        //b1.setBounds(100, 140, 80, 30);
 
         b1.addActionListener((new ActionListener() {
             @Override
@@ -54,12 +57,22 @@ public class PswForgot extends MioFrame implements ActionListener,WindowListener
                     fw.write(t1.getText()+"\t"+t2.getText()+"\n");
                     fw.close();
                     JFrame f = new JFrame();
-                    JOptionPane.showMessageDialog(f, "Registration Completed");
+                    JOptionPane.showMessageDialog(f, "Reset Complete");
                     dispose();
                 }catch(Exception exception){}
             }
         }));
 
+        try {
+            BufferedImage img = ImageIO.read(getClass().getResource("logo.png")); // Change this to your image file path
+            Image dimg = img.getScaledInstance(40, 40, Image.SCALE_SMOOTH);
+            ImageIcon imageIcon = new ImageIcon(dimg);
+            JLabel imageLabel = new JLabel(imageIcon);
+            imageLabel.setBounds(215, 70, 60, 60); // Adjust position and size as needed
+            add(imageLabel);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
         add(l3);
